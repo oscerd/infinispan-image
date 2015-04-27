f286f3935a3c45932eced635860f1d5546e48eca
Andrea Cosentino <ancosen@gmail.com>
2015-04-27 12:52:36 +0200

    Add management user/password test/test

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@
 
 /bin/my_init
 /opt/infinispan-server/bin/standalone.sh -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0
+/opt/infinispan-server/bin/add-user.sh -u test -p "test" -ro supervisor,reader,writer
+
