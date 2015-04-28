ee57741bd32af734125c59bcd0bcbd1fa880c502
Andrea Cosentino <ancosen@gmail.com>
2015-04-28 14:06:37 +0200

    Minor changes to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 /bin/my_init
-/opt/infinispan-server/bin/standalone.sh -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0
 
-if [ "$AUTH" -eq "true" ]; then
+if [ "$AUTH" = "true" ]; then
     /opt/infinispan-server/bin/add-user.sh -u $AUTH_USERNAME -p "$AUTH_PASSWORD" -ro supervisor,reader,writer
 fi
+
+/opt/infinispan-server/bin/standalone.sh -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0
